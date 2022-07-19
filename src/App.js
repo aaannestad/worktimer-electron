c9a7e242f941586adaa97a7a5ef0ebf5df4258fd
aaannestad <a.aannestad@gmail.com>
2022-07-19 12:35:40 -0500

    generating UI elements by looping now works

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import "./App.css";
 import { Button } from "antd";
 import Project from "./project.js";
 
+const testData = {
+  item1: {
+    name: "test",
+    time: "now",
+  },
+  item2: {
+    name: "other",
+    time: "then",
+  },
+};
+
+var projectElements = [];
+
+for (var item in testData) {
+  projectElements.push(<Project name={testData[item].name}></Project>);
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Project name="test"></Project>
+        <>{projectElements}</>
       </header>
     </div>
   );
